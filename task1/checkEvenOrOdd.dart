bbcd6e006ecbbd577931f4a9db716501bb108149
Purnima8555 <110591365+Purnima8555@users.noreply.github.com>
2024-11-16 20:02:27 +0545

    evenOrOdd

---
+++ task1/checkEvenOrOdd.dart
@@ -0,0 +1,12 @@
+void checkEvenOrOdd(int num) {
+  if (num % 2 == 0) {
+    print("$num is an Even number.");
+  } else {
+    print("$num is an Odd number.");
+  }
+}
+
+void main() {
+  int num = 7;
+  checkEvenOrOdd(num);
+}
